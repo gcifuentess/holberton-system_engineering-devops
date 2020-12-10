e3b1fdbcd0c7d086ad28647fd96662851fe55c96
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-10 17:38:13 +0000

    fixed 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,26 @@
-# Install Nginx web server (w/ Puppet)
+# install Nginx web server (w/ Puppet)
 
 exec { 'apt-get_update':
     command => '/usr/bin/sudo /usr/bin/apt-get update -y',
+    before  => Exec['install_nginx'],
 }
 
 exec { 'install_nginx':
     command => '/usr/bin/sudo /usr/bin/apt-get install nginx -y',
+    before  => Exec['index.html'],
 }
 
 exec { 'index.html':
     command => '/usr/bin/sudo /bin/echo Holberton School > /var/www/html/index.html',
+    before  => Exec['exec_3'],
 }
 
-exec { 'redirect':
-    command => '/usr/bin/sudo /bin/sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+exec { 'exec_3':
+  require     => Exec['index.html'],
+  environment => ['GG=google.com permanent'],
+  command     => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me $GG;/" /etc/nginx/sites-enabled/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
 }
 
 exec {'start_nginx':
