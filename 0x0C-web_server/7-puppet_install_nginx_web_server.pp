88b23c543fd8ab810b502b8c8340b081ae8349d8
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-10 15:51:38 +0000

    fixed 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,6 +10,7 @@ exec { 'index.html':
 
 file_line { '/etc/nginx/sites-available/default':
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
   line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
 }
