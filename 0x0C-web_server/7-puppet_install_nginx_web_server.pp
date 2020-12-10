2454843d7eee1d4d9a198118762770a760cd78c5
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-10 17:06:35 +0000

    fixed 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,24 @@
 # Install Nginx web server (w/ Puppet)
 
+exec { 'update':
+  command => 'sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin/'],
+  returns => [0, 1],
+  before  => Package['nginx'],
+}
+
 package { 'nginx':
   ensure => 'installed',
   name   => 'nginx',
 }
 
-file { '/var/www/html/index.hmtl':
+file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
-file_line { '/etc/nginx/sites-enabled/default':
+file_line { '/etc/nginx/sites-available/default':
   ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
