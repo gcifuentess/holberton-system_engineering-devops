ac884e46d907eaaaab5a956708f4e34e43384d30
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-10 04:02:10 +0000

    fixed 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,21 @@
 # Install Nginx web server (w/ Puppet)
 
 exec { 'apt-get_update':
-    command => 'apt-get update -y',
-    path    => '/usr/bin/',
+    command => '/usr/bin/sudo /usr/bin/apt-get update -y',
 }
 
 exec { 'install_nginx':
-    command => 'apt-get install nginx -y',
-    path    => '/usr/bin/',
+    command => '/usr/bin/sudo /usr/bin/apt-get install nginx -y',
 }
 
 exec { 'index.html':
-    command => 'echo Holberton School > /var/www/html/index.html',
-    path    => '/bin/'
+    command => '/usr/bin/sudo /bin/echo Holberton School > /var/www/html/index.html',
 }
 
 exec { 'redirect':
-    command => 'sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    path    => '/bin/',
+    command => '/usr/bin/sudo /bin/sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 }
 
 exec {'start_nginx':
-    command => '/usr/sbin/service nginx start',
+    command => '/usr/bin/sudo /usr/sbin/service nginx start',
 }
-
-# service { 'start_nginx':
-#    ensure => 'running',
-#    name   => 'nginx',
-#}
