20f1d748ac09d6ff715ba95dd1fe470131893779
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-04 21:33:22 +0000

    fix 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,11 @@ def top_ten(subreddit):
     listed for a given subreddit'''
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     my_header = {'User-Agent': 'my-integration/1.2.3'}
-    r = requests.get(url, headers=my_header, allow_redirects=False).json()
-    # nb_subscribers = r.get('data', {}).get('subscribers', 0)
+    r = requests.get(url, headers=my_header, allow_redirects=False)
+    if (r.status_code != 200):
+        print(None)
+        return
+    r = r.json()
     r_data = r.get('data', None)
     if (r_data):
         for i in range(10):
