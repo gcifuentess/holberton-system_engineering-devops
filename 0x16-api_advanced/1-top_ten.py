9e9e967104009e11bfe0e8629bd38ee1dfadc3cd
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-04 21:28:24 +0000

    add 1-top_ten.py 1-main.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Top Ten'''
+import requests
+
+
+def top_ten(subreddit):
+    '''queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit'''
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    my_header = {'User-Agent': 'my-integration/1.2.3'}
+    r = requests.get(url, headers=my_header, allow_redirects=False).json()
+    # nb_subscribers = r.get('data', {}).get('subscribers', 0)
+    r_data = r.get('data', None)
+    if (r_data):
+        for i in range(9):
+            hot_title = r_data['children'][i]['data']['title']
+            print(hot_title)
