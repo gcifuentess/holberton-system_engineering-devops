06fc0a3d5d48169db913a4c6b4d0490460c966bb
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-25 21:31:27 +0000

    fix 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,4 @@
 # fix Security limits
 exec { 'Fix hard limit':
-  command => '/usr/bin/env sed -i s/"holberton hard nofile 4"/"anyuser hard nofile 30000"/ /etc/security/limits.conf'
-}
-
-exec { 'Fix soft limit':
-  command => '/usr/bin/env sed -i s/"holberton soft nofile 5"/"anyuser soft nofile 30000"/ /etc/security/limits.conf'
+  command => '/usr/bin/env sed -i "s/4/30000/; s/5/30000/" /etc/security/limits.conf'
 }
